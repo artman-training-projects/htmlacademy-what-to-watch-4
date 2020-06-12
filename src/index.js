7ef46fe05f0cd53563e04617a87fcaaad741d2f0
Art <ya.Artriches@ya.ru>
2020-06-12 13:47:42 +0300

    add: массив фильмов, и передача их в props

--- src/index.js
+++ src/index.js
@@ -9,9 +9,32 @@ const MoviePoster = {
   YEAR: `2014`,
 };
 
+const films = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`,
+  `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`,
+  `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+];
+
 ReactDom.render(
     <App
       moviePoster = {MoviePoster}
+      films = {films}
     />,
     document.querySelector(`#root`)
 );
