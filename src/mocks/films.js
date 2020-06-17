55190403e3a0f8a8a1c68997e3f06264e3b73768
Art <ya.Artriches@ya.ru>
2020-06-17 20:23:25 +0300

    add: моки films

---
+++ src/mocks/films.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    poster: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    poster: `img/bohemian-rhapsody.jpg`,
+  },
+  {
+    title: `Macbeth`,
+    poster: `macbeth.jpg`,
+  },
+  {
+    title: `Aviator`,
+    poster: `img/aviator.jpg`,
+  },
+  {
+    title: `We need to talk about Kevin`,
+    poster: `img/we-need-to-talk-about-kevin.jpg`,
+  },
+  {
+    title: `What We Do in the Shadows`,
+    poster: `img/what-we-do-in-the-shadows.jpg`,
+  },
+  {
+    title: `Revenant`,
+    poster: `img/revenant.jpg`,
+  },
+  {
+    title: `Johnny English`,
+    poster: `img/johnny-english.jpg`,
+  },
+];
