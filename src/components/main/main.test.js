12b4497fd1efaf7bfb8ead348325614a202b6d3b
Art <ya.Artriches@ya.ru>
2020-06-18 20:50:53 +0300

    fix: snapshots тест компонента Main

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
 import Main from './main.jsx';
-import {MoviePoster, films} from '../../const.js';
+
+const moviePoster = {
+  TITLE: `Seven Years in Tibet`,
+  GENRE: `Drame`,
+  YEAR: `1997`,
+};
+
+const films = [{
+  title: `We need to talk about Kevin`,
+  poster: `img/we-need-to-talk-about-kevin.jpg`,
+},
+{
+  title: `What We Do in the Shadows`,
+  poster: `img/what-we-do-in-the-shadows.jpg`,
+},
+{
+  title: `Revenant`,
+  poster: `img/revenant.jpg`,
+},
+{
+  title: `Johnny English`,
+  poster: `img/johnny-english.jpg`,
+}];
 
 it(`Render Main`, () => {
   const tree = renderer
     .create(
         <Main
-          movieTitle = {MoviePoster.TITLE}
-          movieGenre = {MoviePoster.GENRE}
-          movieYear = {MoviePoster.YEAR}
+          movieTitle = {moviePoster.title}
+          movieGenre = {moviePoster.genre}
+          movieYear = {moviePoster.year}
           films = {films}
           onSmallMovieCardTitleClick = {() => {}}
         />
