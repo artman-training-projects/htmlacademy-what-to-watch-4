61cc6a02302e272845601016f636783b3cdb2b55
Art <ya.Artriches@ya.ru>
2020-06-18 21:18:05 +0300

    add: snapshots тест компонента SmallMovieCard

---
+++ src/components/small-movie-card/small-movie-card.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SmallMovieCard from './small-movie-card.jsx';
+
+const film = {
+  title: `We need to talk about Kevin`,
+  poster: `img/we-need-to-talk-about-kevin.jpg`,
+};
+
+it(`Render SmallMovieCard`, () => {
+  const tree = renderer
+    .create(
+        <SmallMovieCard
+          film = {film}
+          onSmallMovieCardTitleClick = {() => {}}
+          onMouseOver = {() => {}}
+        />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
