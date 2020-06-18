e17fcd6f11c4b2a03f046e90efcf4776542ab22e
Art <ya.Artriches@ya.ru>
2020-06-18 20:44:31 +0300

    fix: snapshots тест компонента App

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
 import App from './app.jsx';
-import {MoviePoster, films} from '../../const.js';
+
+const moviePoster = {
+  TITLE: `Seven Years in Tibet`,
+  GENRE: `Drame`,
+  YEAR: `1997`,
+};
+
+const films = [{
+  title: `We need to talk about Kevin`,
+  poster: `img/we-need-to-talk-about-kevin.jpg`,
+},
+{
+  title: `What We Do in the Shadows`,
+  poster: `img/what-we-do-in-the-shadows.jpg`,
+},
+{
+  title: `Revenant`,
+  poster: `img/revenant.jpg`,
+},
+{
+  title: `Johnny English`,
+  poster: `img/johnny-english.jpg`,
+}];
 
 it(`Render App`, () => {
   const tree = renderer
     .create(
         <App
-          movieTitle = {MoviePoster.TITLE}
-          movieGenre = {MoviePoster.GENRE}
-          movieYear = {MoviePoster.YEAR}
+          movieTitle = {moviePoster.title}
+          movieGenre = {moviePoster.genre}
+          movieYear = {moviePoster.year}
           films = {films}
         />
     )
