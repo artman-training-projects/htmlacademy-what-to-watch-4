9614919b74abb6487b8fc193ddb5f88e70888487
Art <ya.Artriches@ya.ru>
2020-06-21 19:51:18 +0300

    add: описание тестовых данных отдельным файлом

---
+++ src/components/data-for-test.js
@@ -0,0 +1,26 @@
+export const moviePoster = {
+  title: `Seven Years in Tibet`,
+  genre: `Drame`,
+  year: `1997`,
+};
+
+export const films = [{
+  id: `1184`,
+  title: `We need to talk about Kevin`,
+  poster: `img/we-need-to-talk-about-kevin.jpg`,
+},
+{
+  id: `1134`,
+  title: `What We Do in the Shadows`,
+  poster: `img/what-we-do-in-the-shadows.jpg`,
+},
+{
+  id: `1148`,
+  title: `Revenant`,
+  poster: `img/revenant.jpg`,
+},
+{
+  id: `1194`,
+  title: `Johnny English`,
+  poster: `img/johnny-english.jpg`,
+}];
