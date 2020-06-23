506f7459527f33a59165cef408d7186988ff1bfb
Art <ya.Artriches@ya.ru>
2020-06-23 06:22:07 +0300

    fix: компонента MovieCard - проброс props и их использование - создание теста

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import {CustomPropTypes} from '../custom-prop-types.js';
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+  const {film} = props;
+
+  const getRating = (rating) => {
+    if (rating < 3) {
+      return `Bad`;
+    } else if (rating < 5) {
+      return `Normal`;
+    } else if (rating < 8) {
+      return `Good`;
+    } else if (rating < 10) {
+      return `Very good`;
+    } else {
+      return `Awesome`;
+    }
+  };
 
   return (<React.Fragment>
     <section className="movie-card movie-card--full">
       <div className="movie-card__hero">
         <div className="movie-card__bg">
-          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+          <img src={film.bg} alt={film.title} />
         </div>
 
         <h1 className="visually-hidden">WTW</h1>
@@ -29,10 +45,10 @@ const MovieCard = () => {
 
         <div className="movie-card__wrap">
           <div className="movie-card__desc">
-            <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+            <h2 className="movie-card__title">{film.title}</h2>
             <p className="movie-card__meta">
-              <span className="movie-card__genre">Drama</span>
-              <span className="movie-card__year">2014</span>
+              <span className="movie-card__genre">{film.genre}</span>
+              <span className="movie-card__year">{film.year}</span>
             </p>
 
             <div className="movie-card__buttons">
@@ -57,7 +73,7 @@ const MovieCard = () => {
       <div className="movie-card__wrap movie-card__translate-top">
         <div className="movie-card__info">
           <div className="movie-card__poster movie-card__poster--big">
-            <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+            <img src={film.poster} alt={film.title} width="218" height="327" />
           </div>
 
           <div className="movie-card__desc">
@@ -76,19 +92,18 @@ const MovieCard = () => {
             </nav>
 
             <div className="movie-rating">
-              <div className="movie-rating__score">8,9</div>
+              <div className="movie-rating__score">{film.rating}</div>
               <p className="movie-rating__meta">
-                <span className="movie-rating__level">Very good</span>
-                <span className="movie-rating__count">240 ratings</span>
+                <span className="movie-rating__level">{getRating(film.rating)}</span>
+                <span className="movie-rating__count">{film.votes} ratings</span>
               </p>
             </div>
 
             <div className="movie-card__text">
-              <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustaves friend and protege.</p>
-              <p>Gustave prides himself on providing first-class service to the hotels guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustaves lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
+              <p>{film.description}</p>
 
-              <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
-              <p className="movie-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
+              <p className="movie-card__director"><strong>Director: {film.director}</strong></p>
+              <p className="movie-card__starring"><strong>Starring: {film.starring.map((star) => star).join(`, `)} and other</strong></p>
             </div>
           </div>
         </div>
@@ -156,3 +171,7 @@ const MovieCard = () => {
 };
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  film: CustomPropTypes.FILM,
+};
---
+++ src/components/movie-card/movie-card.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import MovieCard from './movie-card.jsx';
+import {films} from '../data-for-test.js';
+
+const moviePoster = films[0];
+
+describe(`MovieCard`, () => {
+  it(`Render MovieCard`, () => {
+    const tree = renderer.create(
+        <MovieCard
+          film = {moviePoster}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
