f901549332b2b1fcd56856590c2ab00b072afda5
Art <ya.Artriches@ya.ru>
2020-06-23 06:57:13 +0300

    fix: компонента MoviesList - обновление тестов

--- src/components/movies-list/movies-list.test.js
+++ src/components/movies-list/movies-list.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import {films} from '../data-for-test.js';
 import MoviesList from './movies-list.jsx';
+import {films} from '../data-for-test.js';
 
 describe(`MoviesList`, () => {
   it(`Render MoviesList`, () => {
     const tree = renderer.create(
         <MoviesList
           films = {films}
-          onSmallMovieCardTitleClick = {() => {}}
+          onSmallMovieCardClick = {() => {}}
         />
     ).toJSON();
 
