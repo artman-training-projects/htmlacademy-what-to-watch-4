61caca4ed54923ea82dca4aeb073ad9ccbd5bd56
Art <ya.Artriches@ya.ru>
2020-07-11 14:28:59 +0300

    fix: запись времени в моках

--- src/mocks/movie-poster.js
+++ src/mocks/movie-poster.js
@@ -6,7 +6,7 @@ export default {
   title: `The Grand Budapest Hotel`,
   genre: `Drame`,
   year: `2014`,
-  time: `1h 39min`,
+  time: `99`,
   description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
   rating: `4.9`,
   votes: `468`,
