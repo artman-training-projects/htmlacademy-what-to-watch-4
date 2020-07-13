17382009bbb22b620ff520d5c3cf42dc14d44a2d
Art <ya.Artriches@ya.ru>
2020-07-13 22:35:43 +0300

    fix: правки по комментариям

--- src/components/video-player-full/video-player-full.jsx
+++ src/components/video-player-full/video-player-full.jsx
@@ -16,6 +16,22 @@ const VideoPlayerFull = (props) => {
     onSetFullScreen,
   } = props;
 
+  const playerToggler = ((currentTime * 100) / duration) + `%`;
+
+  const btnIsPlaying = isPlaying ?
+    <React.Fragment>
+      <svg viewBox="0 0 14 21" width="14" height="21">
+        <use xlinkHref="#pause"></use>
+      </svg>
+      <span>Pause</span>
+    </React.Fragment> :
+    <React.Fragment>
+      <svg viewBox="0 0 19 19" width="19" height="19">
+        <use xlinkHref="#play-s"></use>
+      </svg>
+      <span>Play</span>
+    </React.Fragment>;
+
   return (
     <div className="player">
       {children}
@@ -28,7 +44,7 @@ const VideoPlayerFull = (props) => {
         <div className="player__controls-row">
           <div className="player__time">
             <progress className="player__progress" value={currentTime} max={duration}></progress>
-            <div className="player__toggler" style={{left: ((currentTime * 100) / duration) + `%`}}>Toggler</div>
+            <div className="player__toggler" style={{left: playerToggler}}>Toggler</div>
           </div>
           <div className="player__time-value">{leftTime}</div>
         </div>
@@ -37,19 +53,7 @@ const VideoPlayerFull = (props) => {
           <button type="button" className="player__play"
             onClick={() => onIsPlayingChange()}
           >
-            {isPlaying ?
-              <React.Fragment>
-                <svg viewBox="0 0 14 21" width="14" height="21">
-                  <use xlinkHref="#pause"></use>
-                </svg>
-                <span>Pause</span>
-              </React.Fragment> :
-              <React.Fragment>
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
-                </svg>
-                <span>Play</span>
-              </React.Fragment>}
+            {btnIsPlaying}
           </button>
           <div className="player__name">{film.title}</div>
 
