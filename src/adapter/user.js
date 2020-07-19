c4b2cf4f58f80d2fadfcd6141f4faa6b0dc38392
Art <ya.Artriches@ya.ru>
2020-07-19 11:06:46 +0300

    add: адаптер для данных user

---
+++ src/adapter/user.js
@@ -0,0 +1,10 @@
+import {useReducer} from "react";
+
+const userAdapter = (user) => ({
+  id: user.id,
+  email: user.email,
+  name: user.name,
+  avatarSrc: `https://4.react.pages.academy` + user.avatar_url,
+});
+
+export default userAdapter;
