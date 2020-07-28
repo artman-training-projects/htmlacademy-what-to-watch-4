ec7f0f91918196605e1a30e60468f0b0e48ef721
Art <ya.Artriches@ya.ru>
2020-07-28 11:59:34 +0300

    add: приватный маршрут

---
+++ src/components/private-route.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {Route, Redirect} from 'react-router-dom';
+
+import {Pages} from '../const.js';
+import {getAuthStatus} from '../reducer/user/selector.js';
+
+const PrivateRoute = (props) => {
+  const {auth, exact, path, render} = props;
+
+  const isAuth = auth.status === `AUTH`;
+  const isProgress = auth.isProgress;
+
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={() => {
+        if (isAuth && !isProgress) {
+          return render();
+        } else if (isProgress) {
+          return false;
+        }
+        return <Redirect to={`${Pages.SIGN_IN}`} />;
+      }}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.shape({
+    status: PropTypes.string.isRequired,
+    error: PropTypes.bool.isRequired,
+    isProgress: PropTypes.bool.isRequired,
+  }).isRequired,
+  exact: PropTypes.bool.isRequired,
+  path: PropTypes.string.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: getAuthStatus(state),
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
